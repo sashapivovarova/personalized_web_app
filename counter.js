62f22484f891597f10186f6a9acfce60ba9ec74c
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2025-01-05 21:07:16 +0900

    increment-btn の追加

--- counter.js
+++ counter.js
@@ -1,17 +1,3 @@
-// document.getElementById("count").innerText = 5
-
 let count = 0
 
 console.log(count)
-
-let bonusPoints = 50
-console.log(bonusPoints)
-
-bonusPoints = bonusPoints + 50
-console.log(bonusPoints)
-
-bonusPoints = bonusPoints - 75
-console.log(bonusPoints)
-
-bonusPoints = bonusPoints + 45
-console.log(bonusPoints)
