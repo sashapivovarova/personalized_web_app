3f111a52021197fe924e28ced8a1726f123505e6
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2025-01-06 21:39:41 +0900

    display the count

--- counter.js
+++ counter.js
@@ -1,3 +1,10 @@
+let countEl = document.getElementById("count-el")
+
+console.log(countEl)
+
 let count = 0
 
-console.log(count)
+function increment() {
+  count = count + 1
+  countEl.innerText = count
+}
