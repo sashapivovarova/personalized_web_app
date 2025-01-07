4db6ad8d2a1c2476c1ab3e38d8c71637da05f1b2
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2025-01-07 21:31:51 +0900

    escape の理解

--- counter.js
+++ counter.js
@@ -1,10 +1,10 @@
 let countEl = document.getElementById("count-el")
-
-console.log(countEl)
-
 let count = 0
 
 function increment() {
   count = count + 1
   countEl.innerText = count
 }
+
+let title = document.getElementById("title")
+title.innerText = "I'd like to change \"the title of the page\""
