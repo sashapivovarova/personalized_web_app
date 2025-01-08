99779625df95d4f37cf79072ab4254cf640ef9f7
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2025-01-08 21:34:25 +0900

    plus equal の理解

--- counter.js
+++ counter.js
@@ -2,9 +2,6 @@ let countEl = document.getElementById("count-el")
 let count = 0
 
 function increment() {
-  count = count + 1
+  count += 1
   countEl.innerText = count
 }
-
-let title = document.getElementById("title")
-title.innerText = "I'd like to change \"the title of the page\""
