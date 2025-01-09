222cf3888f2a6c9fa627097e43fb96f7f95bb1a4
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2025-01-09 22:45:14 +0900

    save-btn の追加

--- counter.js
+++ counter.js
@@ -2,6 +2,12 @@ let countEl = document.getElementById("count-el")
 let count = 0
 
 function increment() {
-  count += 1
+  count +=
+    1
   countEl.innerText = count
+}
+
+function save() {
+
+  console.log(count)
 }
