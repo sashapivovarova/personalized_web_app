61574758d9eb07a7d372c19dce68a0f1a566fce8
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2025-01-10 22:31:53 +0900

    Previous entires の表示

--- counter.js
+++ counter.js
@@ -1,3 +1,4 @@
+let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 let count = 0
 
@@ -8,6 +9,6 @@ function increment() {
 }
 
 function save() {
-
-  console.log(count)
+  let countStr = count + " - "
+  saveEl.innerText += countStr
 }
