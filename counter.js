ab3214d88a40a8de76c2a20c9a423d6d8c4e9be6
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2025-01-11 21:55:24 +0900

    count の初期化

--- counter.js
+++ counter.js
@@ -3,12 +3,14 @@ let countEl = document.getElementById("count-el")
 let count = 0
 
 function increment() {
-  count +=
-    1
-  countEl.innerText = count
+  count += 1
+  countEl.textContent = count
 }
 
 function save() {
   let countStr = count + " - "
-  saveEl.innerText += countStr
+  saveEl.textContent += countStr
+
+  count = 0
+  countEl.textContent = count
 }
