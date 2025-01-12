68a84e223b8f5e14ad817bf0627fd8c304205e55
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2025-01-12 20:34:15 +0900

    error-btn の追加

--- counter.js
+++ counter.js
@@ -2,6 +2,14 @@ let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 let count = 0
 
+let errorParagraph = document.getElementById("error")
+console.log(errorParagraph)
+
+function purchase() {
+  console.log("button clicked")
+  errorParagraph.textContent = "Something went wrong, please try again"
+}
+
 function increment() {
   count += 1
   countEl.textContent = count
