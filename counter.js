b509a4647b557cefcfe01450c6b8ddec9ff2a46c
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2025-01-13 20:45:43 +0900

    calculator の追加

--- counter.js
+++ counter.js
@@ -22,3 +22,29 @@ function save() {
   count = 0
   countEl.textContent = count
 }
+
+let num1 = 8
+let num2 = 2
+document.getElementById("num1-el").textContent = num1
+document.getElementById("num2-el").textContent = num2
+let sumEl = document.getElementById("sum-el")
+
+function add() {
+  let result = num1 + num2
+  sumEl.textContent = "Sum: " + result
+}
+
+function subtract() {
+  let result = num1 - num2
+  sumEl.textContent = "Sum: " + result
+}
+
+function divide() {
+  let result = num1 / num2
+  sumEl.textContent = "Sum: " + result
+}
+
+function multiply() {
+  let result = num1 * num2
+  sumEl.textContent = "Sum: " + result
+}
