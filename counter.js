4fa69a7107e89c30ee02322bbff00ac95db213ba
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2025-01-15 20:27:58 +0900

    boolean の理解

--- counter.js
+++ counter.js
@@ -50,13 +50,20 @@ function multiply() {
 }
 
 let firstCard = 10
-let secondCard = 7
-let sum = firstCard + secondCard
+let secondCard = 11
+let sum = firstCard + secondCard + 4
+let hasBlackJack = false
+let isAlive = true
 
 if (sum <= 20) {
   console.log("Do you want to draw a new card? 🙂")
 } else if (sum === 21) {
   console.log("Wohoo! You've got Blackjack! 🥳")
+  hasBlackJack = true
 } else {
   console.log("You're out of the game! 😭")
+  isAlive = false
 }
+
+console.log(hasBlackJack)
+console.log(isAlive)
