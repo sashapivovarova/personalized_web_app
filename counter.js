8401101d317643fcb335097b8a9e8121f0e59152
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2025-01-18 14:41:08 +0900

    btn の追加

--- counter.js
+++ counter.js
@@ -1,69 +1,22 @@
-let saveEl = document.getElementById("save-el")
-let countEl = document.getElementById("count-el")
-let count = 0
-
-let errorParagraph = document.getElementById("error")
-console.log(errorParagraph)
-
-function purchase() {
-  console.log("button clicked")
-  errorParagraph.textContent = "Something went wrong, please try again"
-}
-
-function increment() {
-  count += 1
-  countEl.textContent = count
-}
-
-function save() {
-  let countStr = count + " - "
-  saveEl.textContent += countStr
-
-  count = 0
-  countEl.textContent = count
-}
-
-let num1 = 8
-let num2 = 2
-document.getElementById("num1-el").textContent = num1
-document.getElementById("num2-el").textContent = num2
-let sumEl = document.getElementById("sum-el")
-
-function add() {
-  let result = num1 + num2
-  sumEl.textContent = "Sum: " + result
-}
-
-function subtract() {
-  let result = num1 - num2
-  sumEl.textContent = "Sum: " + result
-}
-
-function divide() {
-  let result = num1 / num2
-  sumEl.textContent = "Sum: " + result
-}
-
-function multiply() {
-  let result = num1 * num2
-  sumEl.textContent = "Sum: " + result
-}
-
 let firstCard = 10
-let secondCard = 11
-let sum = firstCard + secondCard + 4
+let secondCard = 4
+let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
+let message = ""
 
-if (sum <= 20) {
-  console.log("Do you want to draw a new card? 🙂")
-} else if (sum === 21) {
-  console.log("Wohoo! You've got Blackjack! 🥳")
-  hasBlackJack = true
-} else {
-  console.log("You're out of the game! 😭")
-  isAlive = false
+// 2. Create a startGame() function. Move the conditional
+// below (line 11-20) inside the body of the function.
+
+function startGame() {
+  if (sum <= 20) {
+    message = "Do you want to draw a new card?"
+  } else if (sum === 21) {
+    message = "Wohoo! You've got Blackjack!"
+    hasBlackJack = true
+  } else {
+    message = "You're out of the game!"
+    isAlive = false
+  }
+  console.log(message)
 }
-
-console.log(hasBlackJack)
-console.log(isAlive)
