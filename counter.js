6d91e1be9f76371288125458b97e867dc2472336
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2025-01-20 20:20:27 +0900

    sumEl の出力

--- counter.js
+++ counter.js
@@ -7,18 +7,19 @@ let message = ""
 
 // 1. Store the message-el paragraph in a variable called messageEl
 let messageEl = document.getElementById("message-el")
-console.log(messageEl)
+let sumEl = document.getElementById("sum-el")
 
 function startGame() {
+  // 3. Render the sum on the page using this format -> "Sum: 14"
+  sumEl.textContent = "Sum: " + sum
   if (sum <= 20) {
     message = "Do you want to draw a new card?"
   } else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack!"
+    message = "You've got Blackjack!"
     hasBlackJack = true
   } else {
     message = "You're out of the game!"
     isAlive = false
   }
-  // 2. Display the message in the messageEl using messageEl.textContent
   messageEl.textContent = message
 }
