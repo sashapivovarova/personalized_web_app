44381d3119ae1b331e5f629ce512d732bb84a9f3
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2025-01-29 20:50:00 +0900

    getRandomCard の追加

--- counter.js
+++ counter.js
@@ -1,5 +1,5 @@
-let firstCard = 10
-let secondCard = 4
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
 let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackJack = false
@@ -9,6 +9,10 @@ let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 // 2. Store the cards paragraph in a variable called cardsEl
 let cardsEl = document.getElementById("cards-el")
+
+function getRandomCard() {
+  return 5
+}
 
 function startGame() {
   renderGame()
@@ -33,7 +37,7 @@ function renderGame() {
 }
 
 function newCard() {
-  let card = 6
+  let card = getRandomCard()
   sum += card
   cards.push(card)
   renderGame()
