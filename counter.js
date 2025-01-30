68b89738072bf4babc999b147a15ab761b4c822c
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2025-01-30 20:36:38 +0900

    randomNumber の追加

--- counter.js
+++ counter.js
@@ -11,7 +11,7 @@ let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
 function getRandomCard() {
-  return 5
+  return Math.floor(Math.random() * 13) + 1
 }
 
 function startGame() {
