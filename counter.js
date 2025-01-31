dd2638a011a95a2773b242cbbe86e0a9fd33aa51
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2025-01-31 21:27:34 +0900

    getRandomCard の完成

--- counter.js
+++ counter.js
@@ -11,7 +11,14 @@ let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
 function getRandomCard() {
-  return Math.floor(Math.random() * 13) + 1
+  let randomNumer = Math.floor(Math.random() * 13) + 1
+  if (randomNumer > 10) {
+    return 10
+  } else if (randomNumer === 1) {
+    return 11
+  } else {
+    return randomNumer
+  }
 }
 
 function startGame() {
