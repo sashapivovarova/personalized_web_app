db4b8523bc234a8596708e37fc748beacfd97825
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2025-02-07 20:59:48 +0900

    inputBtn の追加

--- work.js
+++ work.js
@@ -2,5 +2,10 @@ function saveLead() {
   console.log("Button clicked!")
 }
 
+let inputBtn = document.getElementById("input-btn")
+
+inputBtn.addEventListener("click", function () {
+  console.log("Button clicked from addEventListener")
+})
 
 
