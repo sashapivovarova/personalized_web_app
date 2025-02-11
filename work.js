6d22c0bcddbc1ccd1cf3447ea01ea41df0e43809
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2025-02-11 21:28:41 +0900

    myLeads の出力

--- work.js
+++ work.js
@@ -1,4 +1,4 @@
-let myLeads = []
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 
@@ -7,4 +7,6 @@ inputBtn.addEventListener("click", function () {
   console.log(myLeads)
 })
 
-
+for (let i = 0; i < myLeads.length; i++) {
+  console.log(myLeads[i])
+}
