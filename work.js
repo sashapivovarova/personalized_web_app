4b676e3d912e04a81f973ba74e647a6125e0174e
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2025-02-21 21:51:19 +0900

    a tag の追加

--- work.js
+++ work.js
@@ -1,16 +1,18 @@
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value)
-  // renderLead()
-  ulEl.innerHTML += "<li>" + inputEl.value + "</li>"
   inputEl.value = ""
+  renderLead()
 })
 
-// function renderLead() {
-//   let listItem = "<li>" + inputEl.value + "</li>"
-//   ulEl.innerHTML += listItem
-// }
+function renderLead() {
+  let listItems = ""
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+  }
+  ulEl.innerHTML = listItems
+}
